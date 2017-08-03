7b745f2ac9062d95d881708f03b2199984690d7d
Juha Kivekäs <guth.smash@gmail.com>
2017-08-03 18:36:40 +0300

    combined tests

--- test/ansi_view_test.py
+++ test/ansi_view_test.py
@@ -1,5 +1,14 @@
 from multidiff import MultidiffModel, AnsiView
 
+def test_sequence_ansi_dump():
+	objs = open("test/pretty_json.txt").read(-1).split('\n\n')
+	m = MultidiffModel(objs)
+	m.diff_sequence()
+	res = AnsiView().dumps(m, printmode='utf8')
+	base = open("test/pretty_json_res.txt").read(-1)
+	assert res == base
+
+
 def test_baseline_ansi_dump():
 	objs = open("test/minimal.txt").read(-1).split('\n')
 	m = MultidiffModel(objs)
--- test/pretty_json_test.py
+++
@@ -1,9 +0,0 @@
-from multidiff import MultidiffModel, AnsiView
-
-def test_sequence_ansi_dump():
-	objs = open("test/pretty_json.txt").read(-1).split('\n\n')
-	m = MultidiffModel(objs)
-	m.diff_sequence()
-	res = AnsiView().dumps(m, printmode='utf8')
-	base = open("test/pretty_json_res.txt").read(-1)
-	assert res == base
