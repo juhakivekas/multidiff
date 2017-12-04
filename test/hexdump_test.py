1fa3abb3e0d15991ec3e080a6bf71b6e606cc6d2
Juha Kivekäs <guth.smash@gmail.com>
2017-12-04 23:07:55 +0200

    fix tests

--- test/hexdump_test.py
+++ test/hexdump_test.py
@@ -7,14 +7,14 @@ def test_single_line_mono():
 	hd = HexdumpView(mono)
 	hd.append(bytes("foobar", "utf8"), "mono")
 	result = hd.final()
-	dump = '000000: 666f6f626172                     |foobar          |\n'
+	dump = '000000: 666f6f626172                     |foobar          |'
 	assert(result == dump)
 
 def test_multiple_line_mono():
 	hd = HexdumpView(mono)
 	hd.append(bytes("0123456789abcdef012345678", "utf8"), "mono")
 	result = hd.final()
-	dump='000000: 30313233343536373839616263646566 |0123456789abcdef|\n000010: 303132333435363738               |012345678       |\n'
+	dump='000000: 30313233343536373839616263646566 |0123456789abcdef|\n000010: 303132333435363738               |012345678       |'
 	assert(result == dump)
 
 def test_single_line_insert():
@@ -22,7 +22,7 @@ def test_single_line_insert():
 	hd.append(bytes("foobar", "utf8"), "insert")
 	result = hd.final()
 	print(result)
-	dump = '000000: \x1b[42m\x1b[37m666f6f626172\x1b[0m                     |\x1b[42m\x1b[37mfoobar\x1b[0m          |\n'
+	dump = '000000: \x1b[42m\x1b[37m666f6f626172\x1b[0m                     |\x1b[42m\x1b[37mfoobar\x1b[0m          |'
 	assert(result == dump)
 
 def test_single_line_insert():
@@ -39,5 +39,5 @@ def test_single_line_insert():
 	dump += '\x1b[44m\x1b[37m36373861\x1b[0m'
 	dump += '                         |'
 	dump += '\x1b[44m\x1b[37m678a\x1b[0m'
-	dump += '            |\n'
+	dump += '            |'
 	assert(result == dump)
