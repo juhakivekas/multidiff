f3a3ea24887fdd61b5ea0749722bdf4306124c60
Juha Kivekäs <guth.smash@gmail.com>
2018-02-09 08:44:28 +0200

    move arguments, rename stuff

--- mdcli.py
+++ mdcli.py
@@ -19,32 +19,39 @@ parser.add_argument('file',
 	nargs='*',
 	help='file or directory to include in multidiff')
 
-parser.add_argument('-m', '--mode',
-	dest='mode',
-	default='sequence',
-	help='mode of operation, either "baseline" or "sequence"')
-
-parser.add_argument('-o','--outformat',
-	dest='outformat',
-	default='hexdump',
-	help='output data format:\n  utf8\n  hex\n  hexdump (default)')
-
-parser.add_argument('-i','--informat',
-	dest='informat',
-	help='input data format:\n  utf8 (stdin default)\n  raw (file and server default)\n  hex\n  json')
+parser.add_argument('-p','--port',
+	dest='port',
+	type=int,
+	help='start a local socket server on the given port')
 
 parser.add_argument('-s','--stdin',
 	dest='stdin',
 	action='store_true',
 	help='read data from stdin, objects split by newlines')
 
-parser.add_argument('-p','--port',
-	dest='port',
-	type=int,
-	help='start a local socket server on the given port')
+parser.add_argument('-m', '--mode',
+	dest='mode',
+	default='sequence',
+	help='mode of operation, either "baseline" or "sequence"')
+
+parser.add_argument('-i','--informat',
+	dest='informat',
+	help='input data format:\n' +
+		'  utf8 (stdin default)\n' +
+		'  raw (file and server default)\n' +
+		'  hex\n'+
+		'  json')
+
+parser.add_argument('-o','--outformat',
+	dest='outformat',
+	default='hexdump',
+	help='output data format:\n' + 
+		'  utf8\n' + 
+		'  hex\n' + 
+		'  hexdump (default)')
 
 parser.add_argument('--html',
-	dest='html',
+	dest='color',
 	action='store_const',
 	const='html',
 	default='ansi',
@@ -53,8 +60,8 @@ parser.add_argument('--html',
 if __name__ == '__main__':
 	args = parser.parse_args()
 	m = MultidiffModel()
-	v = StreamView(m, encoding=args.outformat, mode=args.mode, color=args.html)
-	
+	v = StreamView(m, encoding=args.outformat, mode=args.mode, color=args.color)
+
 	if len(args.file) > 0:
 		informat = args.informat if args.informat else 'raw'
 		files = FileController(m, informat)
